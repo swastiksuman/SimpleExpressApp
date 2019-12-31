44917dc7456289a16018c19a4f140e2cc20849db
Swastik Panda <swastiksuman@gmail.com>
2019-12-31 23:13:45 +0530

    added mongoose

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://swastik:elnino@cluster0-shard-00-00-ufg82.mongodb.net:27017,cluster0-shard-00-01-ufg82.mongodb.net:27017,cluster0-shard-00-02-ufg82.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true }, 
+  (err, db) => {console.log(err)}
+);
+
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel.js');
+const bookRouter = express.Router();
+bookRouter.route('/books').get((req, res) => {
+  Book.find((err, books) => {
+    if(err){
+      return res.send(err);
+    }else{
+      return res.json(books);
+    }  
+  }); 
+}); 
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello! World');
 });
 
 app.listen(port, () => {
-  console.log('Listening on port 3000');
+  console.log(`Listening on port ${port}`);
 });
