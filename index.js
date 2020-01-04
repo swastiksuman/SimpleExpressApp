426bcb9860d05d72de31fdbd94656b46a9bf6659
Swastik Panda <swastiksuman@gmail.com>
2020-01-04 21:37:18 +0530

    added post

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const app = express();
 const db = mongoose.connect('mongodb://swastik:elnino@cluster0-shard-00-00-ufg82.mongodb.net:27017,cluster0-shard-00-01-ufg82.mongodb.net:27017,cluster0-shard-00-02-ufg82.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true }, 
   (err, db) => {console.log(err)}
@@ -9,7 +9,17 @@ const db = mongoose.connect('mongodb://swastik:elnino@cluster0-shard-00-00-ufg82
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel.js');
 const bookRouter = express.Router();
-bookRouter.route('/books').get((req, res) => {
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+bookRouter.route('/books')
+.post((req, res) => {
+  const book = new Book(req.body);
+  book.save();
+  return res.status(201).json(book);
+})
+.get((req, res) => {
   const { query } = req;
   Book.find(query, (err, books) => {
     if(err){
@@ -20,7 +30,8 @@ bookRouter.route('/books').get((req, res) => {
   }); 
 }); 
 
-bookRouter.route('/books/:bookId').get((req, res) => {
+bookRouter.route('/books/:bookId')
+.get((req, res) => {
   const { query } = req;
   Book.findById(req.params.bookId, (err, books) => {
     if(err){
