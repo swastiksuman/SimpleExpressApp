3f904c5af94034ae655c4726b99f3e98c6f55c73
Swastik Panda <swastiksuman@gmail.com>
2020-01-15 08:59:37 +0530

    added unit tests

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,9 +1,14 @@
 
 function booksController(Book) {
     function post(req, res) {
+        if(!req.body.title){
+            res.status(400);
+            return res.send('Title is required')
+        }
         const book = new Book(req.body);
         book.save();
-        return res.status(201).json(book);
+        res.status(201);
+        return res.json(book);
     }
 
     function get(req, res) {
